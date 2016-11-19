1d4c2644477fb21cbc881fa441f059274b5a2f7c
mykolamatsiakh <mmatsiakh@gmail.com>
2016-11-19 18:05:53 +0200

    added firebase dependencies

--- app/src/main/java/com/smarthospital/smarthospital/SignUpActivity.java
+++ app/src/main/java/com/smarthospital/smarthospital/SignUpActivity.java
@@ -2,10 +2,13 @@ package com.smarthospital.smarthospital;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -14,9 +17,20 @@ public class SignUpActivity extends AppCompatActivity {
         return startIntent;
     }
 
+    private TextInputEditText mEmailEditText;
+    private TextInputEditText mPasswordEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        mEmailEditText = (TextInputEditText) findViewById(R.id.email_edit_text);
+        mPasswordEditText=(TextInputEditText) findViewById(R.id.password_edit_text);
+       findViewById(R.id.sign_up_button).setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Toast.makeText(SignUpActivity.this, mEmailEditText.getText(), Toast.LENGTH_SHORT).show();
+           }
+       });
     }
 }
