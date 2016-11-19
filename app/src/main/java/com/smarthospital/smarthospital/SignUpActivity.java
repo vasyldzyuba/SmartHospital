059db7dfc31f1f6e525e5c4b9375f872b463624b
mykolamatsiakh <mmatsiakh@gmail.com>
2016-11-19 17:27:36 +0200

    Merge branch 'master' of /home/mykola/PROJECTS/SmartHospital with conflicts.

--- app/src/main/java/com/smarthospital/smarthospital/SignUpActivity.java
+++ app/src/main/java/com/smarthospital/smarthospital/SignUpActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.Toolbar;
 
 public class SignUpActivity extends AppCompatActivity {
 
