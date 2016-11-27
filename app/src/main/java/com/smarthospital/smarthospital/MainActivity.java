a97e18ac3825d3da0f71cf7738b6dfb65b5b25a3
VikaMi <viktoria.mishchanyn@gmail.com>
2016-11-27 15:00:48 +0200

    added coments to MainActivity

--- app/src/main/java/com/smarthospital/smarthospital/MainActivity.java
+++ app/src/main/java/com/smarthospital/smarthospital/MainActivity.java
@@ -13,31 +13,46 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+//   викликає метод onCreate при створенні або перезапуску активності
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//   задає зовнішній вигляд
         setContentView(R.layout.activity_main);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+//   перевіряє чи зареєстрований такий користувач на FireBase
         if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+
+//       якщо користувача немає, то запускається SignInActivity
             startSignInActivity();
             return;
         }
-
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+//       коли юзер тисне на цю кнопку відбувається LogOut
                 FirebaseAuth.getInstance().signOut();
+
+//       після LogOut стартуємо SignInActivity
                 startSignInActivity();
             }
         });
     }
 
+//   цей метод стартує SignInActivity
     private void startSignInActivity() {
+//        цим методом можна стартанути будь-яку Activity, в нашому випадку SignInActivity
         startActivity(SignInActivity.getStartIntent(MainActivity.this));
+
+//   завершує роботу активності
         finish();
     }
 }
