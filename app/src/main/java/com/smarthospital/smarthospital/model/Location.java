06450aab018220707f0cbd473d26d9e19f6a25f9
mykolamatsiakh <mmatsiakh@gmail.com>
2016-12-04 16:51:17 +0200

    implemented parcelable

--- app/src/main/java/com/smarthospital/smarthospital/model/Location.java
+++ app/src/main/java/com/smarthospital/smarthospital/model/Location.java
@@ -1,14 +1,40 @@
 package com.smarthospital.smarthospital.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by HOME-PC on 27.11.2016.
  */
 
-public class Location {
+
+public class Location implements Parcelable {
 
     public String address;
     public double latitude;
     public double longitude;
+
+    public Location(){
+
+    }
+
+    protected Location(Parcel in) {
+        address = in.readString();
+        latitude = in.readDouble();
+        longitude = in.readDouble();
+    }
+
+    public static final Creator<Location> CREATOR = new Creator<Location>() {
+        @Override
+        public Location createFromParcel(Parcel in) {
+            return new Location(in);
+        }
+
+        @Override
+        public Location[] newArray(int size) {
+            return new Location[size];
+        }
+    };
 
     public String getAddress() {
         return address;
@@ -29,6 +55,18 @@ public class Location {
                 ", latitude=" + latitude +
                 ", longitude=" + longitude +
                 '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(address);
+        parcel.writeDouble(latitude);
+        parcel.writeDouble(longitude);
     }
 }
 
