9bb5212a04435ffda9f8dc6f8912a7746aeef6b7
VikaMi <viktoria.mishchanyn@gmail.com>
2016-12-18 12:09:51 +0200

    added SplashActivity

---
+++ app/src/main/java/com/smarthospital/smarthospital/screen/splashScreen/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.smarthospital.smarthospital.screen.splashScreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.smarthospital.smarthospital.screen.entry.SignInActivity;
+import com.smarthospital.smarthospital.screen.hospitals.HospitalsActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, HospitalsActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
